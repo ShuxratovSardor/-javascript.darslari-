89ab432126e200372db1909f426e447249e465d9
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-04-25 21:28:05 +0500

    Veraybele

---
+++ 1.dars.js/1.javasccript.js
@@ -0,0 +1,31 @@
+ //==== Veraybele ===//
+//====== const, var, let.=====[
+
+// ===let scopka tegishli == //
+// let w = 9;
+// w = 14;
+// console.log(w)//14
+
+// ===========]
+
+// ==== var global===[
+
+//  var s = "salom";
+//  s = "heloow";
+// console.log(s)//heloow
+
+//===========
+
+// ====const uzgarmas====//
+// const c = 76
+// console.log(c) //76 uzgarmedi
+// ==============
+
+//================================]
+
+// ==kiska yozish===[
+
+// let num = 1, c=2, b=3;
+// console.log(num,c,b,);
+
+// ===========]
