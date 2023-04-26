68efa38f6264284bcef90caaa748877eb33f23e2
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-04-27 02:12:59 +0500

    Function

---
+++ 3.dars.js/3.javascript.js
@@ -0,0 +1,53 @@
+ // function turlari //
+
+// Hoisting - doyim yoqorada deklear qilishka aytiladi
+//  veraybele va function expression va arrow function uchuredi  
+
+//============== 
+
+// function expression, function declaration, arrow function
+
+// function decloretion : fuction name = () {}
+// function expression  : let name = () {}
+// arrow function       : let name = () => {}
+
+//===============
+
+// function larga misol //
+
+// function returenga qaytadi //
+
+// function test () {
+//         return 100 ;
+// }
+// console.log(test());
+
+//=============
+
+// function declaration //
+// Hoistinga uchuramedi
+
+// function test (){
+//     console.log('hey');
+// }
+// test()
+
+// =====================
+
+// function expression  //
+// Hoistinga uchuredi
+
+// const hey = function () {
+//     console.log("hey");
+// }
+// hey()
+
+//========================
+
+// Arrow function //
+// Hoistinga uchuredi
+
+// const test = () => {
+//     console.log("Sardor");
+// }
+// test()
