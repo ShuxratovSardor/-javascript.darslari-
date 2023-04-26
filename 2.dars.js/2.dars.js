4032dff8248e8d833bdfd0c0674280a9fe4b4950
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-04-26 05:08:54 +0500

    Data Taypes

---
+++ 2.dars.js/2.dars.js
@@ -0,0 +1,63 @@
+//======== Data Taypes =================[
+// primitiv taqoslaganimizda value qaydaradi
+// non primitiv uzidan link qaytaradi
+
+//Primitive//
+// typeof - taypini tekshirib beradi //
+
+//====== string `` "" ''=====[
+
+// let lop = "`sardor`" //` ` "
+// let bob = "'zur'";   //|" ' ' "
+// let name ='"salom"'; // |' " "  '
+// console.log('sunnat  sardor   "javlon" ' + lop)
+// console.log("sunnat  sardor    'bobur'")
+// console.log(`webbrien academy ${lop + name}  ${lop} ${bob}`)
+// ================================]
+
+//====== Number ==== raqam =[
+// let d = "12"
+// let a = 12;
+// console.log( a)
+//============]
+
+//==== Symbol ===== yunik  id berish uchun ishlatiladi =[
+// symbolda ikta birhil narsa busayam teng bumedi//
+
+// let a = Symbol("text")
+// let b = Symbol("text")
+// console.log(a == b);//false
+// console.log(a === b);//false
+//=======================]
+
+//======= Boolean =======[ true false
+// true = 1;  //teng
+// false = 0; //teng
+// console.log(true == false);//false
+// console.log(true == true )//true
+// console.log(false == false)//true
+//================]
+
+// ====bigInt == katta bugan sonlani ishlatib beradi[
+
+//  console.log(9007199254740992n + 1n)
+// =============]
+
+// ======= undefinet == shikalatam yoq qog'aziyam yuq[
+
+// let text;
+// console.log(text);
+// =====
+// text = " salom";
+// console.log(text)
+// ===========]
+//=========================]
+
+//==== null== qog'ozi bor shikalt yoq =[
+// let a = null;
+// console.log(a);
+//============]
+
+//non primitive//
+
+// Object
