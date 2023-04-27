659ec7245176926be13f2f8520e48dd060815377
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-04-28 00:09:34 +0500

    OR NOT AND

---
+++ 4.dars.js/4.javascript.js
@@ -0,0 +1,63 @@
+// | != |   ignor  / yoq digani /    //
+// != asini qiladi true busa false qaytaradi
+//=========== |!=| < yoq undemas digani |false|true|
+//|!=|prosta teng emas
+//|!==| kat i teng emas
+// let a =1;
+// let b =1;
+// console.log(a  == b);//true
+// console.log(a != b);//false
+//==========================
+
+// =========================================]
+
+// OR //
+//     OR   / yoki digani /  ===  truega ishledi
+//============ || <or belgisi// <ikalasini tekshirib beradi
+// let ceo = false;
+// let admin = true;
+// console.log( true || false)// true
+// console.log("" || null);//null
+// console.log("" || undefined);//undefined
+// console.log(false || "sardor");//sardor
+// console.log(true || "sardor");//true
+// console.log("sardor" || "sunnat");//sardor
+// console.log("" || "begi");//begi
+//==========================]
+
+// AND && / va digani /  ========[ falsega ishledi
+// =============== AND &&
+// ikalasi true busa true qaytaradi
+// let id = true;
+// let false = true;
+// console.log(id && face);//true
+//============ 0, null, indefined.
+//bitasi folse busayam false qaytaradi
+// let id =false;
+// let face = true;
+// console.log(id && face)//false
+// console.log(true && false);//false
+// console.log('' && 0 && true && false);//
+// console.log(0 && true);o//0
+// console.log("sardor" && null);//null
+// console.log(null && undefined);//null
+// console.log(false && null);//false
+// console.log( false  || undefined);//undefined
+//================================]
+
+//===== Nullish ?? false ga ishledi -> , null,undefined keganda keyingisiga utkazib yuboradi ====//
+//==== OR || ,null , undefined, 0, false, NaN.===//
+// console.log(null ?? "web");//web
+// console.log(undefined ?? 1);//1
+// console.log(false || 1);// 1
+// console.log(0 ?? 1 );// 0
+// console.log(12 ?? null);//12
+// console.log(NaN ?? 1);//NaN
+// console.log(false ?? true);//false
+// console.log( false ?? 0);//false
+// console.log(NaN || 1);//1
+// console.log(null || 1);//1
+// console.log(null ?? 1);//1
+// console.log(undefined || 1);//1
+// console.log(undefined ?? 1);//1
+//=============]
