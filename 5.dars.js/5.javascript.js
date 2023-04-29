30900ecf58218631a29a0078ddb1ca10966d3c81
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-04-30 01:15:25 +0500

    if else

---
+++ 5.dars.js/5.javascript.js
@@ -0,0 +1,85 @@
+  //  if else //
+//  if else // shart aperatiri
+//  if, ta  0,  null, undefined <false digani//
+
+// if doyim true, false ga ishledi
+
+// let temp = 4;
+// if (temp <= 10) {
+//   console.log("Havo sovuq");
+// }if (temp > 10) {
+//   console.log("Havo issiq");
+// }
+
+//========================
+
+//can open gmail chiqadi shu teng busa 18 >= 18
+
+//  let age = 20
+//  if(age >= 18){
+//   console.log("can open gmail")
+//  }else{
+//   console.log("can.t open gmail");
+//  }
+//===========================
+
+//============================
+// else if //
+// |else if| nima uchun kire? - tugri javob topildimi undan
+//  qoganini uqib utirmedi.
+//============================
+// agar |if| uzini yozib kitadigan buse uzidan
+// kiyingilarinyam uqib kitadi u  tugri buladimi
+// notug'ri buladimi farqi yuq uqidi.
+// else if  ishlash holatti//
+
+// "", null, undefined, NaN, 0
+
+// let temp = "";
+
+// if (temp === "issiq") {
+//   console.log("havo issiq yengilrok keyining");
+// }
+// else if (temp == "sovuq") {
+//   console.log("Havo sovuq qalinroq kiyinib oling");
+// }
+// else if (temp === "yomgrli") {
+//   console.log("Havo yomgirli soyabon oling");
+// }
+// else if (temp === "qor") {
+//   console.log("Havo qorli qalinroq kiyinib oling");
+// }else{
+//     console.log("nomalum obi havo");
+// }
+
+//=============
+
+// let temp = "issiq";
+
+// if (temp === "issiq") {
+//   console.log("havo issiq yengilrok keyining");
+// }
+//  if (temp == "sovuq") {
+//   console.log("Havo sovuq qalinroq kiyinib oling");
+// }
+//  if (temp === "yomgrli") {
+//   console.log("Havo yomgirli soyabon oling");
+// }
+//  if (temp === "qor") {
+//   console.log("Havo qorli qalinroq kiyinib oling");
+// }else{
+//     console.log("nomalum obi havo");
+// }
+
+// ==========
+
+// ? : //
+//====== turnori operator ? : ======== ? if : else
+
+// ? if digani,// : else digani//
+
+// let gradus = 50
+// gradus < 50 ? console.log("52 cl") : gradus > 50 ? console.log("90 cl") : gradus == 50  ? console.log("nomalum cl") : console.log("not");
+
+//  ? temp < 20
+//  :console.log("sovuq")
