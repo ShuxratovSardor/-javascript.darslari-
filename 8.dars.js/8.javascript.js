90fd412e4ae4f009791bf8939cfb108d1911f3af
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-05-03 04:01:27 +0500

    while,do while

---
+++ 8.dars.js/8.javascript.js
@@ -0,0 +1,40 @@
+// while,do while//
+//=0,null,undefined ,false yurmedi,=//
+//=while qachonki |true| busa ushada ishledi=//
+//  while (0) {
+//  console.log("hey");//hishnarsa chiqmedi
+// }
+//===============
+
+//=cheksiz davom itadi=//
+// while (true ) {
+//   console.log("hey");
+// }
+//=================
+
+//=tepadan boshlab sanab kiladi 3 2 1 console da=//
+// let  i = 3;
+// while (i) {
+//  console.log(i--);
+// }
+//==================
+
+// let i = 4;
+// while (i) {
+//   console.log(`hey`);
+//   i--;
+// }
+// let i = 3
+// do{
+//   console.log(i);
+//   i--
+// }while(i==0)
+
+//javobi//
+//hey
+//hey
+//hey
+//0
+//===============
+
+//============================]
