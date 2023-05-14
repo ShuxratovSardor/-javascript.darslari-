a839609dc3b4122503f78f749f5b704b9988bade
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-05-14 21:02:34 +0500

    new Date

---
+++ 16.dars.js/16.javascrip.js
@@ -0,0 +1,18 @@
+// new Date //
+
+//  let data = new Date()// constractor yasab beradi sana yasab beradi
+//  let dte = Date.now()// 1970 yil dan boshlab hizirgacha bugan sekundini chiqarib beradi
+//  console.log(getTime());//1970 yil dan boshlab hizirgacha bugan sekundini chiqarib beradi
+//  console.log(getDay());// Hafta ni chiqarib beradi
+//  console.log(getDate());// kunlarni chiqarib beradi
+//  console.log(getMonth());// oydi chiqarib  beradi
+//  console.log(getFullYear());// yildi chiqarib beradi
+
+
+//  // soat metodlar //
+
+//  let det = new Date()
+//  console.log(getHours());// sotini chiqarob beradi
+//  console.log(getMinutes());// minutini chiqarib beradi
+//  console.log(getSeconds());// secundini chiqarib beradi
+//  console.log(getMilliseconds());// milliy secundini chiqarib beradui
