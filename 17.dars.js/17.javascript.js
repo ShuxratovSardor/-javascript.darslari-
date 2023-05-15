84112d858e9adc1603a7c6cb8281409fc489fc9f
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-05-15 21:19:19 +0500

    Distructure

---
+++ 17.dars.js/17.javascript.js
@@ -0,0 +1,14 @@
+// Distructure // arry obj
+      // obj //
+// let obj = {name:"Sardor", surname: "Shuxratov", year:"2005"}
+
+// const {name, surname, year} = obj
+
+// console.log(name);//javobi: Sardor
+
+      //arry //
+// let arr = ["sardor", "Shuxartov" ]
+
+// const [bir, ikki, uch] = arr
+
+// console.log(bir);// javobi: Sardor
