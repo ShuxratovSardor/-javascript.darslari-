301bd2af28d5f912f6934a0b85ac6c02ee2d2833
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-05-16 21:40:36 +0500

    JSON

---
+++ 18.dars.js/18.javascript.js
@@ -0,0 +1,29 @@
+// JSON //
+// let obj = {name:"sardor", surname:"shuxratov"}
+// console.log(JSON.stringify(obj));// objecti JSON ga uzgartorib beradi string kurinshiga opkib beradi
+// console.log(JSON.parse()); //JSON di obj utkazib beradi
+// 1 parametr value
+// 2 parametrda dependensid
+// 3 paremetr 2 tab barobar joy tashab bersin
+
+//============
+// 1 parametr value
+// 2 parametrda dependensid
+// 3 paremetr 2 tab barobar joy tashab bersin
+
+// let obj = {
+//   name: "sardor",
+//   surname: "shuxratov",
+//   data: {
+//     founded: 1999,
+//   },
+// };
+// console.log(JSON.stringify(obj, ["name", "surname"], 4));
+
+// javobi:{
+//     "name": "sardor",      
+//     "surname": "shuxratov",
+//     "data": {
+//         "founded": 1999    
+//     }
+// }
