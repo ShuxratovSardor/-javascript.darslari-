477594179db6d59b28ff73ce94e5445c485b0d52
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-06-03 22:20:17 +0500

    DDOS Attac

---
+++ 20.dars.js/20.javascript.js
@@ -0,0 +1,22 @@
+    // DDOS Attac //
+// arrorlani oldini olish uchun 
+// yani hardoyim bekendtan mlumoti opkimasinda 
+// uzimizda bor malumoti chiqarsin yuq bulsa sahranit 
+// qivosinda va yana shu malumot suralganda uzimizda chiqarib
+// bersin sahrnit qivoganini
+
+// function getData () {
+//   return fetch("https://www.youtube.com/").then((res)=>res.json());
+// }
+
+// function getChashe(fnc) {
+//   let cashe = new Map()
+//   return function(x){
+//     if(cashe.has(x)){
+//     return cashe.get(x)
+//     }
+//     let res = fnc();
+//     cashe.set(x,res)
+//   }
+// }
+// getData = getChashe(getData)()
